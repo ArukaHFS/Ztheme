761c2b40225764dede0b7a52b9491d170ca401df
himura95 <himura95@hotmail.com>
2015-08-08 14:54:42 +0200

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Ztheme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 633);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ouvrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(996, 633);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Enregistrer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 27);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(239, 604);
+            this.checkedListBox1.TabIndex = 6;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(717, 633);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Actualiser";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(140, 634);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(84, 20);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 637);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Durée en millisecondes :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Effets :";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(258, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fond";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(333, 32);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Element 1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(411, 32);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(72, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Element 2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(489, 32);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(72, 17);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Element 3";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(567, 32);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(72, 17);
+            this.radioButton5.TabIndex = 17;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Element 4";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(1077, 32);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(52, 17);
+            this.radioButton6.TabIndex = 18;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Video";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(1136, 32);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(82, 17);
+            this.radioButton7.TabIndex = 19;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Cadre video";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(646, 32);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(72, 17);
+            this.radioButton8.TabIndex = 20;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Element 5";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(492, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Importer ressource";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(737, 58);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(239, 23);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "Supprimer ressource";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(481, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Nom du theme :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(570, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 20);
+            this.textBox1.TabIndex = 24;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(258, 87);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(960, 540);
+            this.webBrowser1.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 659);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ztheme";
+            this.Load += new System.EventHandler(this.Ztheme_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ztheme
+{
+    public partial class Form1 : Form
+    {
+        string fond;
+        string element1;
+        string element2;
+        string element3;
+        string element4;
+        string element5;
+        string video;
+        string cadreVideo;
+        string theme;
+        string destFile;
+        string baseHtml;
+        string nomRom = "nom de la rom";
+        string curDir = Directory.GetCurrentDirectory(); //recupère le dossier ou se trouve l'application
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Création du fichier index.html avec sa structure de base
+            baseHtml = "<html>\n<head>\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<script src=\"fabric.js\"></script>\n</head>\n<body>\n<canvas id=\"Canvas\" width=\"960\" height=\"540\">\n</canvas>\n<script>\nvar canvas = new fabric.Canvas('Canvas');\n</script>\n</body></html>";
+            System.IO.StreamWriter file = new System.IO.StreamWriter("index.html");
+            file.WriteLine(baseHtml);
+            file.Close();
+            //Affiche index.html
+            webBrowser1.Navigate(String.Format("file:///{0}/index.html", curDir));
+        }
+
+        private void Ztheme_Load(object sender, EventArgs e)
+        {
+            var items = checkedListBox1.Items;
+            items.Add("Rebond");
+            items.Add("Défilement vers la gauche");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog(); //création d'une fenetre d'exploration 
+            f.Title = "Choisir le fichier à importer";
+            if (radioButton1.Checked) //si "fond" est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF"; //filtre le type de fichier autorisé
+                f.ShowDialog(); //affiche la boite de dialogue
+                fond = f.FileName;
+                destFile = @"media\Background" + Path.GetExtension(fond);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(fond, destFile, true);
+                //Ajoute l'image background dans index.html 
+                string stringBuffer ="";
+                using(StreamReader reader = new StreamReader("index.html"))
+                {
+                    string line =  reader.ReadLine();
+
+                    while(line != null)
+                    {
+                        stringBuffer += line + "\n";
+                        if (line.Contains("<canvas"))
+                        {
+                            stringBuffer += "<img src=\"media\\Background.png\" id=\"Background\">\n";
+                        }
+                        if (line.Contains("var canvas = new fabric.Canvas('Canvas');"))
+                        {
+                            stringBuffer += "var imgElement0 = document.getElementById('Background');\nvar imgInstance0 = new fabric.Image(imgElement0, {\n    width:960,\n    height:540\n});\ncanvas.add(imgInstance0);\n";
+                        }
+                        line = reader.ReadLine();
+                    }
+                    reader.Close();
+                }
+                System.IO.StreamWriter file = new System.IO.StreamWriter("index.html");
+                file.WriteLine(stringBuffer);
+                file.Close();
+                webBrowser1.Refresh(); //rafraichi la vue
+            }
+            else if (radioButton2.Checked) //si element 1 est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF";
+                f.ShowDialog();
+                element1 = f.FileName;
+                destFile = @"media\element1" + Path.GetExtension(element1);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(element1, destFile, true);
+                //Ajoute l'image element1 dans index.html 
+                string stringBuffer = "";
+                using (StreamReader reader = new StreamReader("index.html"))
+                {
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        stringBuffer += line + "\n";
+                        if (line.Contains("<canvas"))
+                        {
+                            stringBuffer += "<img src=\"media\\element1.png\" id=\"element1\">\n";
+                        }
+                        if (line.Contains("var canvas = new fabric.Canvas('Canvas');"))
+                        {
+                            stringBuffer += "var imgElement1 = document.getElementById('element1');\nvar imgInstance1 = new fabric.Image(imgElement1, {\n    left:100,\n    top:100,\n    angle: 30\n});\ncanvas.add(imgInstance1);\n";
+                        }
+                        line = reader.ReadLine();
+                    }
+                    reader.Close();
+                }
+                System.IO.StreamWriter file = new System.IO.StreamWriter("index.html");
+                file.WriteLine(stringBuffer);
+                file.Close();
+                webBrowser1.Refresh(); //rafraichi la vue
+            }
+            else if (radioButton3.Checked) //si element 2 est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF";
+                f.ShowDialog();
+                element2 = f.FileName;
+                destFile = @"media\element2" + Path.GetExtension(element2);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(fond, destFile, true);
+            }
+            else if (radioButton4.Checked) //si element 3 est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF";
+                f.ShowDialog();
+                element3 = f.FileName;
+                destFile = @"media\element3" + Path.GetExtension(element3);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(fond, destFile, true);
+            }
+            else if (radioButton5.Checked) //si element 4 est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF";
+                f.ShowDialog();
+                element4 = f.FileName;
+                destFile = @"media\element4" + Path.GetExtension(element4);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(fond, destFile, true);
+            }
+            else if (radioButton8.Checked) //si element 5 est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF";
+                f.ShowDialog();
+                element5 = f.FileName;
+                destFile = @"media\element5" + Path.GetExtension(element5);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(fond, destFile, true);
+            }
+            else if (radioButton6.Checked) //si video est coché
+            {
+                f.Filter = "Videos(*.flv;*.mp4)|*.FLV;*.MP4";
+                f.ShowDialog();
+                video = f.FileName;
+            }
+            else if (radioButton7.Checked) //si cadre video est coché
+            {
+                f.Filter = "Images(*.PNG;*.GIF)|*.PNG;*.GIF";
+                f.ShowDialog();
+                cadreVideo = f.FileName;
+                destFile = @"media\cadreVideo" + Path.GetExtension(cadreVideo);
+                System.IO.Directory.CreateDirectory("media");
+                System.IO.File.Copy(fond, destFile, true);
+            }
+            else //si rien n'est coché
+            {
+                MessageBox.Show("Merci de cocher un element");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog(); //création d'une fenetre d'exploration 
+            f.Title = "Choisir le fichier à importer";
+            if (f.ShowDialog() == DialogResult.OK) //si fond est coché
+            {
+                f.Filter = "Images(*.ZIP)|*.ZIP"; //filtre le type de fichier autorisé
+                f.ShowDialog(); //affiche la boite de dialogue
+                theme = f.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Erreur");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) //si fond est coché
+            {
+                System.IO.File.Delete(@"media\Background.png");
+            }
+            else if (radioButton2.Checked) //si element 1 est coché
+            {
+                System.IO.File.Delete(@"media\element1.png");
+            }
+            else if (radioButton3.Checked) //si element 2 est coché
+            {
+                System.IO.File.Delete(@"media\element2.png");
+            }
+            else if (radioButton4.Checked) //si element 3 est coché
+            {
+                System.IO.File.Delete(@"media\element3.png");
+            }
+            else if (radioButton5.Checked) //si element 4 est coché
+            {
+                System.IO.File.Delete(@"media\element4.png");
+            }
+            else if (radioButton8.Checked) //si element 5 est coché
+            {
+                System.IO.File.Delete(@"media\element5.png");
+            }
+            else if (radioButton6.Checked) //si video est coché
+            {
+                MessageBox.Show("cas particulier");
+            }
+            else if (radioButton7.Checked) //si cadre video est coché
+            {
+                System.IO.File.Delete(@"media\cadreVideo.png");
+            }
+            else //si rien n'est coché
+            {
+                MessageBox.Show("Merci de cocher un element");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string startPath = @".\";
+            //string zipPath = @".\"+ nomRom;
+            //ZipFile.CreateFromDirectory(startPath, zipPath);    
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            nomRom = textBox1.Text;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
